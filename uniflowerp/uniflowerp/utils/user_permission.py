8a6757169a390e7219621570d82c8c82af5df656
Gokulnath17 <m.gokulnath17012002@gmail.com>
2023-12-17 18:35:09 +0530

    feat: user permission creation

--- uniflowerp/uniflowerp/utils/user_permission.py
+++ uniflowerp/uniflowerp/utils/user_permission.py
@@ -1,10 +1,77 @@
 import frappe
 
-def validate(sel, event):
-    pass
+def validate(self, event):
+
+    if not self.get("__islocal") and self.allow == "Company":
+
+        frappe.msgprint("User Permission updation moved to background job, It will complete shortly.")
+
+        frappe.enqueue(changing_user_permission, self = self, queue = "long")
 
 def after_insert(self, event):
-    pass
 
+    frappe.msgprint("User Permission creation moved to background job, It will complete shortly.")
+
+    frappe.enqueue(create_user_permission, for_value = self.for_value, main_user = self.user, queue = "long")
+   
 def on_trash(self, event):
-    pass
+
+    if self.allow == "Company":
+
+        frappe.msgprint("User Permission deletion moved to background job, It will complete shortly.")
+
+        frappe.enqueue(deleting_user_permission, self = self, queue = "long")
+
+def create_user_permission(for_value, main_user, current_doc = None):
+
+    if current_doc:
+
+        user_list = frappe.get_all("User Permission", {"allow": "Company", "for_value": for_value, "name": ["!=", current_doc]}, ["user"], pluck = "user")
+
+    else:
+
+        user_list = frappe.get_all("User Permission", {"allow": "Company", "for_value": for_value}, ["user"], pluck = "user")
+
+    for user in user_list:
+
+        new_doc = frappe.new_doc("User Permission")
+        new_doc.user = main_user
+        new_doc.allow = "User"
+        new_doc.for_value = user
+
+        new_doc.db_insert()
+
+def changing_user_permission(self):
+
+    old_user = frappe.db.get_value("User Permission", self.name, "user")
+
+    old_for_value =  frappe.db.get_value("User Permission", self.name, "for_value")
+    
+    if self.user != old_user or self.for_value != old_for_value:
+
+        user_permission_list = frappe.get_all("User Permission", {"user": old_user, "allow": "User"}, ["name"], pluck = "name")
+
+        user_permission_list += frappe.get_all("User Permission", {"for_value": old_user, "allow": "User"}, ["name"], pluck = "name")
+
+        for user_permission in user_permission_list:
+
+            frappe.delete_doc("User Permission", user_permission, delete_permanently = True)
+
+        create_user_permission(for_value = self.for_value, main_user = self.user, current_doc = self.name)
+
+        new_doc = frappe.new_doc("User Permission")
+        new_doc.user = self.user
+        new_doc.allow = "User"
+        new_doc.for_value = self.user
+
+        new_doc.db_insert()
+
+def deleting_user_permission(self):
+
+    user_permission_list = frappe.get_all("User Permission", {"user": self.user, "allow": "User"}, ["name"], pluck = "name")
+
+    user_permission_list += frappe.get_all("User Permission", {"for_value": self.for_value, "allow": "User"}, ["name"], pluck = "name")
+
+    for user_permission in user_permission_list:
+
+        frappe.delete_doc("User Permission", user_permission, delete_permanently = True)
